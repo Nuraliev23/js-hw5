64eb3936c9e909fae26354233a98976f44b94f6e
Nuraliev11 <rahimrahim4a@gmail.com>
2025-02-15 11:40:20 +0300

    first commit

---
+++ practice.js
@@ -0,0 +1,68 @@
+
+
+// // ctask - 1
+
+// function modif(str, num){
+//  if(num == 1){
+//         return str
+
+//  }    
+//       return str + modif(str.at(-1),num-1)
+
+// }
+// console.log(modif("hello", 3));
+// console.log(modif("hey", 6));
+// console.log(modif("excuse me what?", 5));
+
+
+// // ctask - 2
+
+// function modif(str1){
+//  let strfirst = str1.charAt(0)
+//  let strend = str1.at(-1)
+//  return strfirst + strend
+// }
+// console.log(modif("hello"));
+// console.log(modif("hey"));
+// console.log(modif("excuse me what?"));
+
+
+// // ctask - 3
+
+// function toInt(str1){
+
+// return +str1
+// }
+// function tostr(str){
+//     return str.toString()
+// }
+// console.log(typeof toInt("77"));
+// console.log(typeof tostr(32));
+// console.log(typeof tostr(151));
+
+// // ctask - 4
+
+// function toInt(str1){
+// if(str1.at(-1) == 's'){
+//     return true 
+// }
+// else {
+//     return false
+// }
+// }
+// console.log(toInt("changes"));
+// console.log(toInt("sdsd"));
+// console.log(toInt("sdsdds"));
+
+
+// // ctask - 5
+
+// function toInt(str1){
+   
+//     let resultstr1 = str1.toUpperCase()   
+//    return resultstr1
+
+//     }
+//     console.log(toInt("changes"));
+//     console.log(toInt("sdsd"));
+//     console.log(toInt("sdsdds"));
